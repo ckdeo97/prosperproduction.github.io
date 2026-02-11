6cf10fbfd3f0b3b31b5d2c5f8bdedfea1ecb1909
ckdeo97 <ckdeo97@gmail.com>
2026-02-11 21:56:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-ㅁㄴㅇ
+document.querySelectorAll(".portfolio-card").forEach(card=>{
+card.addEventListener("click",()=>{
+const videoId=card.dataset.video;
+modal.style.display="flex";
+modalIframe.src=
+"https://player.vimeo.com/video/"+videoId+
+"?autoplay=1&muted=1&title=0&byline=0&portrait=0&badge=0&controls=1&dnt=1";
+});
+});
